2daa153d33ac63b5f437ecb4ebe64b8e1e67ec85
Cristina <crisruedap@gmail.com>
2021-08-29 17:31:51 -0500

    Reorder content

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ function changeState(){
       navMenu.classList.toggle('nav-menu-open');
   let menuOpen = document.getElementById('menu-toggle');
       menuOpen.classList.toggle('menu-open');
-
-  if (navMenu.classList.contains('nav-menu-open')) {
-    menuOpen.setAttribute("aria-label", "Cerrar menú");
-  } else {
-    menuOpen.setAttribute("aria-label", "Abrir menú");
-  }
 };
 
 
