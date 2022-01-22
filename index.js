86fb0fad3bdbe41246ab6f9c014360006ab64188
CrisRuedaP <crisruedap@gmail.com>
2022-01-21 21:13:19 -0500

    Update Project

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 function changeState(){
-  let navMenu = document.getElementById('nav-menu');
-      navMenu.classList.toggle('nav-menu-open');
-  let menuOpen = document.getElementById('menu-toggle');
-      menuOpen.classList.toggle('menu-open');
+    let navContainer = document.querySelector('#nav-container');
+    let navMenu = document.getElementById('nav-menu');
+        navMenu.classList.toggle('nav-menu-open');
+    let menuOpen = document.getElementById('menu-toggle');
+        menuOpen.classList.toggle('menu-open');
 
-  if (navMenu.classList.contains('nav-menu-open')) {
-    menuOpen.setAttribute("aria-label", "Cerrar menú");
-  } else {
-    menuOpen.setAttribute("aria-label", "Abrir menú");
-  }
+    
+    if (navMenu.classList.contains('nav-menu-open')) {
+        menuOpen.setAttribute("aria-expanded", "true");
+        navContainer.removeAttribute("hidden");
+    } 
+
+    if(!menuOpen.classList.contains('menu-open')){
+        menuOpen.setAttribute("aria-expanded", "false");
+        navContainer.setAttribute("hidden", "true");
+    }
 };
+  
+document.addEventListener('DOMContentLoaded', (e) => {
+    let ratio = window.devicePixelRatio || 1;
+    let w = screen.width * ratio;
+    const navContainer = document.querySelector('#nav-container');
+
+    if(w < 960) {
+        navContainer.setAttribute("aria-hidden", "true");
+    }else{
+        navContainer.removeAttribute('aria-hidden')
+        navContainer.removeAttribute("hidden");
+    }
+});
 
 
 
