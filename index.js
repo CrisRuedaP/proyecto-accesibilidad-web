51f35e0c37c8358dfbb2ac7cf2a15e2bb8e99f3f
CrisRuedaP <crisruedap@gmail.com>
2022-01-21 21:15:47 -0500

    Fix file format

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-function changeState(){
-    let navContainer = document.querySelector('#nav-container');
-    let navMenu = document.getElementById('nav-menu');
-        navMenu.classList.toggle('nav-menu-open');
-    let menuOpen = document.getElementById('menu-toggle');
-        menuOpen.classList.toggle('menu-open');
+function changeState() {
+  let navContainer = document.querySelector("#nav-container");
+  let navMenu = document.getElementById("nav-menu");
+  navMenu.classList.toggle("nav-menu-open");
+  let menuOpen = document.getElementById("menu-toggle");
+  menuOpen.classList.toggle("menu-open");
 
-    
-    if (navMenu.classList.contains('nav-menu-open')) {
-        menuOpen.setAttribute("aria-expanded", "true");
-        navContainer.removeAttribute("hidden");
-    } 
+  if (navMenu.classList.contains("nav-menu-open")) {
+    menuOpen.setAttribute("aria-expanded", "true");
+    navContainer.removeAttribute("hidden");
+  }
 
-    if(!menuOpen.classList.contains('menu-open')){
-        menuOpen.setAttribute("aria-expanded", "false");
-        navContainer.setAttribute("hidden", "true");
-    }
-};
-  
-document.addEventListener('DOMContentLoaded', (e) => {
-    let ratio = window.devicePixelRatio || 1;
-    let w = screen.width * ratio;
-    const navContainer = document.querySelector('#nav-container');
+  if (!menuOpen.classList.contains("menu-open")) {
+    menuOpen.setAttribute("aria-expanded", "false");
+    navContainer.setAttribute("hidden", "true");
+  }
+}
 
-    if(w < 960) {
-        navContainer.setAttribute("aria-hidden", "true");
-    }else{
-        navContainer.removeAttribute('aria-hidden')
-        navContainer.removeAttribute("hidden");
-    }
+document.addEventListener("DOMContentLoaded", (e) => {
+  let ratio = window.devicePixelRatio || 1;
+  let w = screen.width * ratio;
+  const navContainer = document.querySelector("#nav-container");
+
+  if (w < 960) {
+    navContainer.setAttribute("aria-hidden", "true");
+  } else {
+    navContainer.removeAttribute("aria-hidden");
+    navContainer.removeAttribute("hidden");
+  }
 });
-
-
-
-
-
